bb49902a67ca3c8ede086fd876df6f557e688417
Kate Hedgpeth <katehedgpeth@gmail.com>
2017-12-19 11:58:31 -0500

    show all red line vehicles

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ function shapes_url() {
   return api_url + "/shapes?filter%5Broute%5D=Red&include=route,stops" + get_date_filter();
 }
 
+function vehicles_url() {
+  return api_url + "/vehicles?filter%5Broute%5D=Red&include=trip,stop" + get_date_filter();
+}
+
 function get_date_filter() {
   var date = new Date();
   // return "&filter%5Bdate%5D="+ "2018" +"-"+ "01" +"-"+ "08" + "&api_key=" + MBTA_API_KEY;
@@ -24,25 +28,57 @@ function promise_request(url) {
   });
 }
 
-function is_shuttle_shape(shape) { return shape.id.includes("Quincy"); }
+function is_shuttle_shape(shape) { return true; }
 
-function draw_shuttle_route(map, included) {
-  return function do_draw_shuttle_route(shuttle, i) {
+function is_shuttle_vehicle(stop_ids) {
+  return function do_is_shuttle_vehicle(vehicle) {
+    return true
+  }
+}
+
+function draw_shape(map, included) {
+  return function do_draw_shape(shape, i) {
     const color = i == 0 ? "#FF0000" : "#000000";
-    const polyline = decode_polyline(shuttle.attributes.polyline)
+    const polyline = decode_polyline(shape.attributes.polyline)
                        .map(points => { return {lat: points[0], lng: points[1]} });
     new google.maps.Polyline({
       path: polyline,
       strokeColor: color,
-      strokeOpacity: 0.5,
+      strokeOpacity: 0.1,
       map: map
     });
-    shuttle.relationships.stops.data.map(render_stop(map, included, color, i));
+    shape.relationships.stops.data.map(draw_stop(map, included, color));
   }
 }
 
-function render_stop(map, included, color, i) {
-  return function do_render_stop(stop_data) {
+function draw_vehicle(map, included) {
+  return function do_draw_vehicle(vehicle, i) {
+    const stop = included.find(data => data.id == vehicle.relationships.stop.data.id);
+    const vehicle_title = ["Vehicle",
+                           vehicle.attributes.label,
+                           vehicle.attributes.current_status.toLowerCase().split("_").join(" "),
+                           stop.attributes.name].join(" ");
+    const color = stop.attributes.name.split(" - ").pop() == "Outbound" ? "#FF0000" : "#000000";
+    new google.maps.Marker({
+      position: {
+        lat: vehicle.attributes.latitude,
+        lng: vehicle.attributes.longitude
+      },
+      title: vehicle_title,
+      icon: {
+        path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+        strokeColor: "#FF0000",
+        scale: 3,
+        rotation: vehicle.attributes.bearing
+      },
+      draggable: false,
+      map: map
+    });
+  }
+}
+
+function draw_stop(map, included, color) {
+  return function do_draw_stop(stop_data) {
     const stop = included.find(included_stop => { return included_stop.id == stop_data.id });
     return new google.maps.Marker({
       position: {
@@ -53,8 +89,7 @@ function render_stop(map, included, color, i) {
       icon: {
         path: google.maps.SymbolPath.CIRCLE,
         strokeColor: color,
-        scale: 3,
-        rotation: 180 * -i
+        scale: 3
       },
       draggable: false,
       map: map,
@@ -64,14 +99,22 @@ function render_stop(map, included, color, i) {
 }
 
 function init_map() {
-  promise_request(shapes_url()).then(data => {
-    const map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 13,
-      center: {lat: 42.241241, lng: -71.005297}
-    });
-    const json  = JSON.parse(data)
-    json.data
-        .filter(is_shuttle_shape)
-        .forEach(draw_shuttle_route(map, json.included));
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: {lat: 42.241241, lng: -71.005297}
   });
+  window.vehicle_map = map;
+  Promise.all([promise_request(shapes_url()), promise_request(vehicles_url())]).then(data => {
+    const json = data.map(str => JSON.parse(str));
+    console.log("json", json);
+    const all_shapes = json[0];
+    const vehicles = json[1];
+    const trips = vehicles.included.slice(0).filter(item => item.type == "trip");
+    const shapes = {};
+    trips.forEach(trip => {
+      shapes[trip.relationships.shape.data.id] = shapes[trip.relationships.shape.data.id] || all_shapes.data.find(shape => shape.id == trip.relationships.shape.data.id)
+    });
+    Object.values(shapes).forEach(draw_shape(map, all_shapes.included.slice(0)));
+    vehicles.data.forEach(draw_vehicle(map, vehicles.included.slice(0)));
+  })
 }
