a496049ecb457646e99be3989985f6dd16c58aef
Kate Hedgpeth <katehedgpeth@gmail.com>
2017-12-28 15:14:31 -0500

    custom Vehicle class

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 
-const routes = "Shuttle005"
-
-function VehicleLabel(vehicle, stop, map) {
-  this.vehicle_ = vehicle;
-  this.attributes_ = vehicle.attributes;
-  this.stop_ = stop;
-  this.map_ = map;
-  this.div_ = null;
+function Vehicle(vehicle, included, map) {
+  this.vehicle_id_ = vehicle.id;
+  this.route_ = included.find(data => data.id == vehicle.relationships.route.data.id);
+  console.log("route", this.route_);
 
   this.setMap(map);
+
+  this.stop_ = null;
+  this.div_ = null;
+  this.marker_ = new google.maps.Marker({
+    map: map
+  });
+
+  this.update(vehicle, included);
 }
 
 function shapes_url() {
-  return ENV.V3_API_URL + "/shapes?filter[route]=" + routes + "&include=route,stops&api_key=" + ENV.MBTA_API_KEY + get_date_filter();
+  return ENV.V3_API_URL + "/shapes?filter[route]=" + shape_route() + "&include=stops&api_key=" + ENV.MBTA_API_KEY + get_date_filter();
 }
 
 function vehicles_url() {
-  return ENV.V3_API_URL + "/vehicles?filter[route]=" + "Red" + "&include=trip,stop&api_key=" + ENV.MBTA_API_KEY + get_date_filter();
+  return ENV.V3_API_URL + "/vehicles?filter[route]=" + vehicle_route() + "&include=route,stop&api_key=" + ENV.MBTA_API_KEY + get_date_filter();
+}
+
+function shape_route() {
+  return "Shuttle005";
+}
+
+function vehicle_route() {
+  // return "Shuttle005"
+  return "202,210,212"
+
 }
 
 function schedules_url() {
@@ -25,10 +39,9 @@ function schedules_url() {
   let minute = now.getMinutes() + 1;
   hour = hour < 10 ? ("0" + hour) : hour;
   minute = minute < 10 ? ("0" + minute) : minute;
-  return ENV.V3_API_URL + "/schedules?filter[route]=" + routes +
+  return ENV.V3_API_URL + "/schedules?filter[route]=" + shape_route() +
                               "&filter[min_time]=" + hour + minute +
                               "&filter[max_time]=" + hour + minute +
-                              "&include=route,stop" +
                               "&api_key=" + ENV.MBTA_API_KEY +
                               get_date_filter();
 }
@@ -70,35 +83,11 @@ function draw_shape(shape, map, included, stop_hash) {
   }
 }
 
-function draw_vehicle(vehicle, map, included) {
-
-  const stop = find_vehicle_stop(vehicle, included);
-  const label = new VehicleLabel(vehicle, stop, map);
-  const marker = new google.maps.Marker({
-    position: {
-      lat: vehicle.attributes.latitude,
-      lng: vehicle.attributes.longitude
-    },
-    icon: {
-      path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-      strokeColor: vehicle_color(vehicle.relationships.route.data.id),
-      scale: 5,
-      rotation: vehicle.attributes.bearing,
-      labelOrigin: label_origin(vehicle.id)
-    },
-    map: map
-  });
-  return {
-    marker: marker,
-    label: label
-  }
-}
-
 function draw_stop(map, included, stop_hash) {
   return function do_draw_stop(stop_data) {
     if (!stop_hash[stop_data.id]) {
       const stop = included.find(included_stop => { return included_stop.id == stop_data.id });
-      const marker = new google.maps.Marker({
+      stop_hash[stop_data.id] = new google.maps.Marker({
         position: {
           lat: stop.attributes.latitude,
           lng: stop.attributes.longitude
@@ -117,14 +106,6 @@ function draw_stop(map, included, stop_hash) {
         zIndex: 1000,
         id: stop.id
       });
-      const info = new google.maps.InfoWindow({
-        content: stop.attributes.name
-      })
-      marker.addListener("click", () => { info.open(map, marker); });
-      stop_hash[stop_data.id] = {
-        marker: marker,
-        label: info
-      }
     }
   }
 }
@@ -176,14 +157,6 @@ function label_origin(id) {
   }
 }
 
-function find_vehicle_stop(vehicle, stops) {
-  return stops.find(data => data.id == vehicle.relationships.stop.data.id);
-}
-
-
-function vehicle_color(_route_id) {
-  return "#FF0000";
-}
 
 function add_shape(shape_hash, map, included, stop_hash) {
   return function do_add_shape(new_shape) {
@@ -200,9 +173,18 @@ function load_map_data(map, shape_hash, vehicle_hash, stop_hash) {
     const new_vehicles = JSON.parse(data[0]);
     const schedules = JSON.parse(data[1]);
     const new_shapes = JSON.parse(data[2]);
-    new_shapes.data.slice(0).forEach(add_shape(shape_hash, map, new_shapes.included, stop_hash));
-    Object.keys(vehicle_hash).forEach(update_vehicle_hash(vehicle_hash, new_vehicles));
-    new_vehicles.data.forEach(add_new_vehicle(vehicle_hash, map, new_vehicles.included.slice(0)))
+    if (new_shapes && new_shapes.data) {
+      new_shapes.data.slice(0).forEach(add_shape(shape_hash, map, new_shapes.included, stop_hash));
+    } else {
+      console.error("unexpected result for new_shapes", new_shapes);
+    }
+
+    if (new_vehicles && new_vehicles.data && new_vehicles.included) {
+      Object.keys(vehicle_hash).forEach(update_vehicle_hash(vehicle_hash, new_vehicles));
+      new_vehicles.data.forEach(add_new_vehicle(vehicle_hash, map, new_vehicles.included.slice(0)))
+    } else {
+      console.error("unexpected result for new_vehicles", new_vehicles);
+    }
 
     window.setTimeout(function(){ load_map_data(map, shape_hash, vehicle_hash, stop_hash) }, 3000);
   })
@@ -212,23 +194,10 @@ function update_vehicle_hash(vehicle_hash, new_vehicles) {
   return id => {
     const new_data = new_vehicles.data.find(vehicle => vehicle.id == id);
     if (new_data) {
-      const lat_lng = new google.maps.LatLng(new_data.attributes.latitude, new_data.attributes.longitude);
-      vehicle_hash[id].marker.setPosition(lat_lng)
-      vehicle_hash[id].marker.setIcon({
-        path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-        strokeColor: vehicle_color(new_data.relationships.route.data.id),
-        scale: 5,
-        rotation: new_data.attributes.bearing
-      });
-      const stop = find_vehicle_stop(new_data, new_vehicles.included)
-      vehicle_hash[id].label.stop_ = stop;
-      vehicle_hash[id].label.attributes_ = new_data.attributes;
-      vehicle_hash[id].label.draw();
-      // vehicle_hash[id].setTitle(vehicle_title(new_data, stop))
-    } else {
-      vehicle_hash[id].marker.setMap(null);
-      vehicle_hash[id].label.setMap(null);
-      delete vehicle_hash[id];
+      vehicle_hash[id].update(new_data, new_vehicles.included.slice(0))
+    } else if (vehicle_hash[id]) {
+      vehicle_hash[id].setMap(null);
+      vehicle_hash[id] = null;
     }
   }
 }
@@ -236,58 +205,107 @@ function update_vehicle_hash(vehicle_hash, new_vehicles) {
 function add_new_vehicle(vehicle_hash, map, included) {
   return new_vehicle => {
     if (!vehicle_hash[new_vehicle.id]) {
-      vehicle_hash[new_vehicle.id] = draw_vehicle(new_vehicle, map, included)
+      vehicle_hash[new_vehicle.id] = new Vehicle(new_vehicle, included, map)
     }
   }
 }
 
 function init_map() {
-  VehicleLabel.prototype = new google.maps.OverlayView();
+  Vehicle.prototype = new google.maps.OverlayView();
 
-  VehicleLabel.prototype.onAdd = function() {
-    var div = document.createElement('div');
-    div.classList.add("vehicle__label");
+  Vehicle.prototype.onAdd = function() {
+    const div = document.createElement("div");
+    div.id = this.vehicle_id_
+    div.classList.add("vehicle");
 
-    // // Create the img element and attach it to the div.
-    // const img = document.createElement('img');
-    // img.src = this.image_;
-    // img.style.width = '100%';
-    // img.style.height = '100%';
-    // img.style.position = 'absolute';
-    // div.appendChild(img);
+    const label_div = document.createElement("div");
+    label_div.classList.add("vehicle__label");
 
-    this.div_ = div;
+    div.appendChild(label_div);
 
-    // Add the element to the "overlayLayer" pane.
+    this.divs_ = {
+      container: div,
+      label: label_div
+    }
+
     const panes = this.getPanes();
     panes.overlayLayer.appendChild(div);
   }
 
-  VehicleLabel.prototype.draw = function() {
-    const overlayProjection = this.getProjection();
-    const lat_lng = new google.maps.LatLng(this.attributes_.latitude, this.attributes_.longitude);
-    const loc = overlayProjection.fromLatLngToDivPixel(lat_lng);
-    this.div_.style.left = loc.x + "px";
-    this.div_.style.top = loc.y + "px";
-    this.div_.textContent = this.label_text();
+  Vehicle.prototype.draw = function() {
+    this.update_marker();
+    if (this.divs_) {
+      this.divs_.label.textContent = this.label_text();
+      const overlayProjection = this.getProjection();
+      if (overlayProjection) {
+        const loc = overlayProjection.fromLatLngToDivPixel(this.get_position());
+        this.divs_.container.style.left = loc.x + "px";
+        this.divs_.container.style.top = loc.y + "px";
+      }
+    }
   };
 
-  VehicleLabel.prototype.remove = function() {
-    this.div_.parentElement.removeChild(this.div_);
+  Vehicle.prototype.onRemove = function() {
+    this.divs_.container.parentElement.removeChild(this.divs_.container);
+    this.marker_.setMap(null);
+    this.marker_ = null;
     return false;
   }
 
-  VehicleLabel.prototype.label_text = function() {
-  return ["Vehicle",
-          this.attributes_.label,
-          this.attributes_.current_status.toLowerCase().split("_").join(" "),
-          this.stop_.attributes.name].join(" ");
+  Vehicle.prototype.icon_opts = function() {
+    return {
+      path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+      strokeColor: this.get_color(),
+      scale: 5,
+      rotation: this.attributes_.bearing
+    }
+  }
+
+  Vehicle.prototype.get_color = function() {
+    switch (this.route_.id) {
+      default:
+        return "#FF0000";
+    }
+  }
+
+  Vehicle.prototype.get_position = function() {
+    return new google.maps.LatLng(this.attributes_.latitude, this.attributes_.longitude);
+  }
+
+  Vehicle.prototype.update_stop = function(stops) {
+    this.stop_ = stops.find(data => data.id == this.relationships_.stop.data.id);
+  }
+
+  Vehicle.prototype.update_marker = function() {
+    this.marker_.setPosition(this.get_position());
+    this.marker_.setIcon(this.icon_opts());
+  }
+
+  Vehicle.prototype.update = function(new_data, stops) {
+    this.attributes_ = new_data.attributes;
+    this.relationships_ = new_data.relationships;
+
+    this.update_stop(stops);
+
+    this.draw();
+  }
+
+  Vehicle.prototype.label_text = function() {
+    return ["Route",
+            this.route_.attributes.short_name,
+            "(" + this.route_.attributes.direction_names[this.attributes_.direction_id] + ")",
+            // "Vehicle",
+            // this.attributes_.label,
+            this.attributes_.current_status.toLowerCase().split("_").join(" "),
+            this.stop_.attributes.name].join(" ");
   }
 
   const beale_at_library = {lat: 42.266671, lng: -71.017924}
   const map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
     center: beale_at_library,
+    mapTypeControl: false,
+    streetViewControl: false,
     styles: [{
       featureType: 'poi',
       stylers: [{visibility: 'off'}]
